86bf1ee54c3c714773cbefa33be2a994e098983a
Tim Fierens <t.fierens@amsterdam.nl>
2022-04-14 15:09:12 +0200

    added missing afwijzingReasons

--- src/model/afwijzing.functions.ts
+++ src/model/afwijzing.functions.ts
@@ -18,10 +18,17 @@ const MARKET_FULL: IAfwijzingReason = {
     code: 4,
     message: 'Alle marktplaatsen zijn reeds ingedeeld.',
 };
-//TODO: missing afwijzingReasons
+const VPL_POSITION_NOT_AVAILABLE: IAfwijzingReason = {
+    code: 5,
+    message: 'De vaste plaatsen voor de vpl zijn niet beschikbaar.',
+};
+const PREF_NOT_AVAILABLE: IAfwijzingReason = {
+    code: 6,
+    message: 'Geen geschikte locatie gevonden met huidige voorkeuren.',
+};
 
 export const getAfwijzingReason = (reasonCode: number) => {
-    const reasons = [BRANCHE_FULL, ADJACENT_UNAVAILABLE, MINIMUM_UNAVAILABLE, MARKET_FULL];
+    const reasons = [BRANCHE_FULL, ADJACENT_UNAVAILABLE, MINIMUM_UNAVAILABLE, MARKET_FULL, VPL_POSITION_NOT_AVAILABLE, PREF_NOT_AVAILABLE];
     return reasons.find(reason => reason.code === reasonCode);
 };
 
