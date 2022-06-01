ae8322ebc2f8536f1eff5b51d869beea0650b82e
Pim <pim.visser@amsterdam.nl>
2022-06-01 13:29:08 +0200

    Add json fixtures for obstakel, plaatseigenschap, markt, marktConfiguratie

--- bdm/src/fixtures/index.ts
+++ bdm/src/fixtures/index.ts
@@ -1,1 +1,7 @@
-export * as genericBranches from './genericBranches.json'
+const genericBranches = require('./genericBranches.json')
+const markt = require('./markt.json')
+const marktConfiguratie = require('./marktConfiguratie.json')
+const obstakels = require('./obstakels.json')
+const plaatseigenschappen = require('./plaatseigenschappen.json')
+
+export { genericBranches, markt, marktConfiguratie, obstakels, plaatseigenschappen }
