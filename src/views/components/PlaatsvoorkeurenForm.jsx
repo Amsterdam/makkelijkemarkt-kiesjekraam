8037a865ce2f2a3f66888e543d4a19907b61f447
Tim Fierens <timfi@tobifaf.nl>
2022-06-23 15:42:44 +0200

    fix import

--- src/views/components/PlaatsvoorkeurenForm.jsx
+++ src/views/components/PlaatsvoorkeurenForm.jsx
@@ -1,7 +1,6 @@
-import isEb from '../../domain-knowledge';
 import PropTypes from 'prop-types';
 import React from 'react';
-const { plaatsSort, isVastOfExp } = require('../../domain-knowledge.ts');
+const { plaatsSort, isVastOfExp, isEb } = require('../../domain-knowledge.ts');
 const { getDefaultVoorkeur } = require('../../model/voorkeur.functions');
 const MarktplaatsSelect = require('./MarktplaatsSelect');
 const Button = require('./Button');
