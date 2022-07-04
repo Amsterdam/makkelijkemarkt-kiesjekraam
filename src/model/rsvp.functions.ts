b2f6463575d820c556003d746ada09a1ac31b658
Harm Manders <h.manders@amsterdam.nl>
2022-07-04 14:51:21 +0200

    patroon -> pattern

--- src/model/rsvp.functions.ts
+++ src/model/rsvp.functions.ts
@@ -75,14 +75,14 @@ export const groupAanmeldingenPerMarktPerWeek = (
     return aanmeldingenPerMarktPerWeek;
 };
 
-export const rsvpPatroonPerMarkt = (
+export const rsvpPatternPerMarkt = (
     markten: MMMarkt[],
     rsvpPatronen: IRsvpPattern[]
 ) : {[key: string]: IRsvpPattern} => {
     const marktIds : number[] = markten.map(markt => +markt.id);
 
     // Filter RsvpPatterns for given markets
-    const rsvpPatronenInMarkten = rsvpPatronen.filter(rsvpPatroon => marktIds.includes(+rsvpPatroon.markt));
+    const rsvpPatronenInMarkten = rsvpPatronen.filter(rsvpPattern => marktIds.includes(+rsvpPattern.markt));
     // Maps RsvpPattern to markt {marktId: pattern, ...}
     return rsvpPatronenInMarkten.reduce((prev, cur)  => ({...prev, [cur.markt]: cur}), {});
 }
