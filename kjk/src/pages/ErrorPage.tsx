f9abf8b64db992468d6767bbd94bfab5cacf8865
Pim <pim.visser@amsterdam.nl>
2022-09-01 00:11:52 +0200

    Improve Error page

--- kjk/src/pages/ErrorPage.tsx
+++ kjk/src/pages/ErrorPage.tsx
@@ -1,7 +1,22 @@
+import { Alert, Button } from 'antd'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const ErrorPage = () => {
-  return <h1>Error</h1>
+  const history = useHistory()
+  return (
+    <Alert
+      message="Error"
+      showIcon
+      description="De gevraagde actie kon niet uitgevoerd worden"
+      type="error"
+      action={
+        <Button size="large" onClick={history.goBack}>
+          Vorige pagina
+        </Button>
+      }
+    />
+  )
 }
 
 export default ErrorPage
