4d191a03444fbbc160cc4aca3c0034194c1c21d5
Tim Fierens <tim@tobifaf.nl>
2022-09-28 10:54:07 +0200

    fix indelings logs

--- src/views/IndelingsLogsPage.tsx
+++ src/views/IndelingsLogsPage.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import LogLine from './components/LogLine';
+import LogLine from './components/LogLine.jsx';
 
 export default class IndelingsLogsPage extends React.Component {
     public render() {
         const data = this.props["data"];
         return (
             data.map((line, i) => (
-                <LogLine key={"logline"+i} 
+                <LogLine key={"logline"+i}
                          type={line["level"]}
                          message={line["message"]}/>
             ))
