e8083ed7791369b39304fe85b6f43a4aa2c8fb1a
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2023-01-24 15:35:33 +0100

    Show ondernemer name and status in indeling stats page

--- src/views/IndelingStatsPage.jsx
+++ src/views/IndelingStatsPage.jsx
@@ -4,14 +4,16 @@ const Page = require('./components/Page.jsx');
 const Content = require('./components/Content.jsx');
 const Header = require('./components/Header.jsx');
 
-const Allocations = (props) => {
+const Allocations = props => {
     return props.children.map(allocation => {
         const style = {
             backgroundColor: allocation.arePrefsMet ? 'aliceblue' : 'white',
-        }
+        };
         return (
             <tr>
                 <td>{allocation.sollicitatieNummer}</td>
+                <td>{allocation.description}</td>
+                <td>{allocation.status}</td>
                 <td>{allocation.branche}</td>
                 <td style={style}>{allocation.plaatsvoorkeuren.join(', ')}</td>
                 <td>{allocation.anywhere ? 'True' : 'False'}</td>
@@ -19,25 +21,31 @@ const Allocations = (props) => {
                 <td>{allocation.minimum}</td>
                 <td>{allocation.maximum}</td>
             </tr>
-        )
-    })
-}
+        );
+    });
+};
 
 class HomePage extends React.Component {
     render() {
         const style = {
             textAlign: 'right',
-        }
-        const prefsMetCount = this.props.allocations.filter(allocation => allocation.arePrefsMet).length
+        };
+        const prefsMetCount = this.props.allocations.filter(allocation => allocation.arePrefsMet).length;
         return (
             <Page>
                 <Header />
                 <Content>
-                    <h2>Indeling stats {this.props.markt.naam} op {this.props.marktDate}</h2>
-                    <p>Ondernemers met plaatsen uit voorkeuren {prefsMetCount} van {this.props.allocations.length}</p>
+                    <h2>
+                        Indeling stats {this.props.markt.naam} op {this.props.marktDate}
+                    </h2>
+                    <p>
+                        Ondernemers met plaatsen uit voorkeuren {prefsMetCount} van {this.props.allocations.length}
+                    </p>
                     <table style={style}>
                         <thead>
                             <th>sollicitatieNummer</th>
+                            <th>naam</th>
+                            <th>status</th>
                             <th>branche</th>
                             <th>plaatsvoorkeuren</th>
                             <th>anywhere</th>
