235beaf4ca9f4ba0bb048ad61f659fb62cc2228a
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2023-07-06 15:04:16 +0200

    Add more actions to marktmeesterApp intermediate api endpoint
    Marktmeester endpoint createDagvergunning and cleanup
    
    Add getDagvergunning action in marktmeester app
    
    Merge getOndernemerByPasUid in getOndernemer
    
    Also log action when dispatch in Marktmeester app is finished
    
    Add queryString in marktmeesterApp
    
    Add deleteDagvergunning action in MarkMeester dispatch
    
    getVervanger method not needed

--- src/routes/marktmeesterApp.js
+++ src/routes/marktmeesterApp.js
@@ -3,13 +3,19 @@ import express, { NextFunction, Request, Response } from 'express';
 import { mmConfig } from '../makkelijkemarkt-api';
 
 const router = express.Router();
+const INVALID_ACTION = {
+    status: 400,
+    data: 'Invalid action',
+};
 
 router.post('/dispatch', async (req, res) => {
     try {
         const { status, data = {} } = await dispatch(req.body, req.headers.usertoken);
+        console.log('happy', { status, action: req.body.action });
         res.status(status).json(data);
     } catch (error) {
-        res.status(error.response.status).json(error.response.data.error);
+        console.log('error', error.response?.data);
+        res.status(error.response?.status || 500).json(error.response?.data.error);
     }
 });
 
@@ -20,6 +26,10 @@ const dispatch = async (body, userToken) => {
     let response = {};
     const headers = userToken ? { authorization: `Bearer ${userToken}` } : {};
     const api = getApi(headers);
+    const { marktId, sollId, pasUid, erkenningsnummer, dagvergunning, id } = data;
+    let queryString = new URLSearchParams(data).toString();
+    queryString = queryString ? '?' + queryString : '';
+    console.log(queryString);
 
     switch (body.action) {
         case 'login':
@@ -37,14 +47,47 @@ const dispatch = async (body, userToken) => {
                 },
             };
 
-        case 'getOndernemerByPasUid':
-            return await api.get(`koopman/pasuid/${data.pasUid}`);
+        case 'getMarkt':
+            if (marktId) {
+                return await api.get(`flex/markt/${marktId}`);
+            }
+            return await api.get(`markt/`);
+
+        case 'getSollicitatie':
+            return await api.get(`sollicitaties/markt/${marktId}?listOffset=0&listLength=1000`);
+
+        case 'getOndernemer':
+            if (pasUid) {
+                return await api.get(`koopman/pasuid/${pasUid}`);
+            }
+            if (erkenningsnummer) {
+                return await api.get(`koopman/erkenningsnummer/${erkenningsnummer}`);
+            }
+            if (sollId && marktId) {
+                return await api.get(`koopman/markt/${marktId}/sollicitatienummer/${sollId}`);
+            }
+            return INVALID_ACTION;
+
+        case 'findOndernemer':
+            if (erkenningsnummer) {
+                return await api.get(`koopman/?erkenningsnummer=${erkenningsnummer}`);
+            }
+            return INVALID_ACTION;
+
+        case 'createDagvergunning':
+            if (dagvergunning) {
+                return await api.post(`/flex/dagvergunning/`, dagvergunning);
+            }
+            return INVALID_ACTION;
+
+        case 'deleteDagvergunning':
+            return await api.delete(`/dagvergunning/${id}`);
+
+        case 'getDagvergunning':
+            return await api.get(`dagvergunning/${queryString}`);
 
         default:
-            return {
-                status: 400,
-                data: 'Invalid action',
-            };
+            return INVALID_ACTION;
     }
 };
 
