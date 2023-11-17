8b8af6293806679ff9882d16b517ab6cf78f90b2
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2023-11-17 11:36:22 +0100

    simplify creating redis client

--- src/concept-queue.ts
+++ src/concept-queue.ts
@@ -1,5 +1,5 @@
 import Queue from 'bee-queue';
-import * as redis from 'redis';
+import { RedisClient } from 'redis-client';
 
 export const ALLOCATION_MODE_CONCEPT = 'concept';
 export const ALLOCATION_MODE_SCHEDULED = 'scheduled';
@@ -11,27 +11,9 @@ export class ConceptQueue {
 
     constructor() {
 
-        const redisHost: string = process.env.REDIS_HOST;
-        const redisPort: string = process.env.REDIS_PORT;
-        const redisPassword: string = process.env.REDIS_PASSWORD;
-
         this.dispatcher_config = {
             prefix: this.prefix,
-            redis: redis.createClient({
-                legacyMode: true,
-                url: `rediss://${redisHost}:${redisPort}`,
-                password: redisPassword,
-                retry_strategy: function (options) {
-                    console.log('Redis retry_strategy')
-                    console.log(options.error?.code)
-                    if (options.error && (options.error.code === 'ECONNREFUSED' || options.error.code === 'NR_CLOSED')) {
-                        // Try reconnecting after 5 seconds
-                        return 5000;
-                    }
-                    // reconnect after
-                    return 3000;
-                },
-            }),
+            redis: new RedisClient().getAsyncClient(),
             isWorker: false,
         };
     }
