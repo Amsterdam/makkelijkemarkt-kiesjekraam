82e8b365a0b107e8a7625d8f8ff4acad99981b86
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2023-11-17 12:25:13 +0100

    pass object instead of create client

--- src/concept-queue.ts
+++ src/concept-queue.ts
@@ -10,10 +10,26 @@ export class ConceptQueue {
     name = 'allocation';
 
     constructor() {
-
+        const redisHost: string = process.env.REDIS_HOST;
+        const redisPort: string = process.env.REDIS_PORT;
+        const redisPassword: string = process.env.REDIS_PASSWORD;
         this.dispatcher_config = {
             prefix: this.prefix,
-            redis: new RedisClient().getAsyncClient(),
+            redis: {
+                legacyMode: true,
+                url: `rediss://${redisHost}:${redisPort}`,
+                password: redisPassword,
+                retry_strategy: function (options) {
+                    console.log('Redis retry_strategy')
+                    console.log(options.error?.code)
+                    if (options.error && (options.error.code === 'ECONNREFUSED' || options.error.code === 'NR_CLOSED')) {
+                        // Try reconnecting after 5 seconds
+                        return 5000;
+                    }
+                    // reconnect after
+                    return 3000;
+                },
+            },
             isWorker: false,
         };
     }
