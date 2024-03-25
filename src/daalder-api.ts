5f32fe8cb9edb4962cc903a175bec31d083aa3c7
Harm <h.manders@amsterdam.nl>
2024-03-25 15:25:30 +0100

    use defined api url to comply with allowed hosts

--- src/daalder-api.ts
+++ src/daalder-api.ts
@@ -9,7 +9,8 @@ requireEnv('MM_RAH_MM_RAH_SERVICE_PORT');
 
 export const daalderConfig = {
   // MM_RAH_MM_RAH_SERVICE_HOST and PORT defined by helm/kubernetes. When service name changes, this var should be changed.
-  baseUrl: `${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
+  // baseUrl: `${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
+  baseUrl: process.env.DAALDER_API_URL,
   authToken: `Token: ${process.env.DAALDER_API_USER_TOKEN}`
 }
 
