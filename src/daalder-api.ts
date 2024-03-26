6415bbba468de000548bb03692432b22bb57b4de
Harm <h.manders@amsterdam.nl>
2024-03-26 16:13:57 +0100

    TOKEN TYPO

--- src/daalder-api.ts
+++ src/daalder-api.ts
@@ -11,7 +11,7 @@ export const daalderConfig = {
   // MM_RAH_MM_RAH_SERVICE_HOST and PORT defined by helm/kubernetes. When service name changes, this var should be changed.
   baseUrl: `http://${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
   // baseUrl: process.env.DAALDER_API_URL,
-  authToken: `Token: ${process.env.DAALDER_API_USER_TOKEN}`
+  authToken: `Token ${process.env.DAALDER_API_USER_TOKEN}`
 }
 
 const getApi = (): AxiosInstance => {
@@ -30,7 +30,7 @@ export const getAllocation = async (data: Object): Promise<Object> => {
   try {
     const response = await axios({
       method: "post",
-      headers: { "Authorization": daalderConfig.authToken, "Accept-Encoding": "gzip, deflate", "Connection":"keep-alive" },
+      headers: { "Authorization": daalderConfig.authToken },
       url: `${daalderConfig.baseUrl}/allocation/allocate/`,
       data: {"data": data}
     });
