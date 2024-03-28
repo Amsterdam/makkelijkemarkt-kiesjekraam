4e98855957408d3a526dcba017073f99f370790a
Harm <h.manders@amsterdam.nl>
2024-03-28 10:52:07 +0100

    Fixed typo

--- src/daalder-api.ts
+++ src/daalder-api.ts
@@ -8,7 +8,8 @@ requireEnv('MM_RAH_MM_RAH_SERVICE_PORT');
 
 
 export const daalderConfig = {
-  baseUrl: process.env.DAALDER_API_URL,
+  // MM_RAH_MM_RAH_SERVICE_HOST and PORT defined by helm/kubernetes. When service name changes, this var should be changed.
+  baseUrl: `${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
   authToken: `Token: ${process.env.DAALDER_API_USER_TOKEN}`
 }
 
