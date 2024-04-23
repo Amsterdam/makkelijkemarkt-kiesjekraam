bbd1a1d7f5c2554bc77aa86256e4e38d8c48c654
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2024-04-23 15:28:58 +0200

    force secure connection

--- src/mail.js
+++ src/mail.js
@@ -4,6 +4,7 @@ import ReactDOMServer from 'react-dom/server';
 const transport = nodemailer.createTransport({
     host: process.env.MAILER_HOST,
     port: process.env.MAILER_PORT || '587',
+    secure: true,
     auth: {
         user: process.env.MAILER_USER,
         pass: process.env.MAILER_PASSWORD
