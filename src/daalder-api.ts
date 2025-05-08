1c0f54b6d7d222c544dadb58795a679f154fde77
Harm Manders <h.manders@amsterdam.nl>
2025-05-08 11:18:14 +0200

    remove old getAllocation method

--- src/daalder-api.ts
+++ src/daalder-api.ts
@@ -5,35 +5,10 @@ requireEnv('DAALDER_API_USER_TOKEN');
 requireEnv('MM_RAH_MM_RAH_SERVICE_HOST');
 requireEnv('MM_RAH_MM_RAH_SERVICE_PORT');
 
+// MM_RAH_MM_RAH_SERVICE_HOST and PORT defined by helm/kubernetes. When service name changes, this var should be changed.
 export const daalderConfig = {
-           // MM_RAH_MM_RAH_SERVICE_HOST and PORT defined by helm/kubernetes. When service name changes, this var should be changed.
-           baseUrl: `http://${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
-           authToken: `Token ${process.env.DAALDER_API_USER_TOKEN}`,
-       };
-
-export const getAllocation = async (data: Object): Promise<Object> => {
-    try {
-            const response = await axios({
-                method: 'post',
-                headers: { Authorization: daalderConfig.authToken },
-                url: `${daalderConfig.baseUrl}/allocation/allocate/`,
-                data: { data: data },
-            });
-
-            if (response.status >= 200 && response.status < 300) {
-                return response.data;
-            } else {
-                return new Error('Api request failed');
-            }
-        } catch (error) {
-        console.log('ERROR: ', error);
-        if (error.response) {
-            console.error('API error response:', error.response.data);
-        } else {
-            console.error('Api request error message', error.message);
-        }
-        return error;
-    }
+    baseUrl: `http://${process.env.MM_RAH_MM_RAH_SERVICE_HOST}:${process.env.MM_RAH_MM_RAH_SERVICE_PORT}`,
+    authToken: `Token ${process.env.DAALDER_API_USER_TOKEN}`,
 };
 
 // New Daalder API client
@@ -43,7 +18,7 @@ const api = axios.create({
         Authorization: daalderConfig.authToken,
         'Content-Type': 'application/json',
     },
-    timeout: 10000,
+    timeout: 10000, // 10 seconds timeout
 });
 
 api.interceptors.response.use(
@@ -52,23 +27,18 @@ api.interceptors.response.use(
         if (error.response) {
             console.error('Daalder API error response:', error.response.data);
         } else {
-                   console.error('API request error message', error.message);
-               }
+            console.error('API request error message', error.message);
+        }
         throw new Error('Daalder API Requst failed');
     },
 );
 
-export const generateAllocation = async (data: Object): Promise<Object> =>
-    await api.post('/allocation/allocate/', { data });
+export const getAllocation = async (data: Object): Promise<Object> =>
+    await api.post('/allocation/allocate/', { data: { data } });
 
 export const updateOndernemerKjkEmail = async (email: string, erkenningsNummer: string): Promise<Object> => {
     console.log('Updating email for ondernemer', email, erkenningsNummer);
-    // return a dummy promise
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.log('Dummy promise resolved');
-            resolve({ status: 'success' });
-        }, 1000);
-    });
+    const response = await api.post('/kiesjekraam/update-kjk-email/', { email, erkenningsNummer });
+    console.log('Response from updateOndernemerKjkEmail:', response);
 };
-    // await api.post('/ondernemer/update-kjk-email/', { email, erkenningsNummer });
+
