e93698aa254280f77c7ae309df29ef6f763fdfc4
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2025-10-15 15:07:17 +0200

    TEMP point api to localhost:3000 for AanwezigheidsPage

--- kjk/src/constants.ts
+++ kjk/src/constants.ts
@@ -1,5 +1,6 @@
-export const MM_API_BASE_ORIGIN = process.env.REACT_APP_KJK_API_ORIGIN || ''
-export const MM_API_BASE_URL = `${MM_API_BASE_ORIGIN}/api`
+export const MM_API_BASE_ORIGIN = 'http://localhost:3000' // process.env.REACT_APP_KJK_API_ORIGIN || ''
+// export const MM_API_BASE_URL = `${MM_API_BASE_ORIGIN}/api`
+export const MM_API_BASE_URL = `${MM_API_BASE_ORIGIN}/daalder`
 
 export const MM_API_QUERY_CONFIG = {
   retry: 1,
