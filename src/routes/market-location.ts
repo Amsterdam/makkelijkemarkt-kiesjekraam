8e885fae54f2e2e7b57d0e78cfff5dd8f8cbaa95
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2025-12-02 16:07:58 +0100

    Connect plaatsvoorkeuren page to Daalder api

--- src/routes/market-location.ts
+++ src/routes/market-location.ts
@@ -1,12 +1,21 @@
 import {
+    // deletePlaatsvoorkeurenByMarktAndKoopman,
+    // getIndelingVoorkeur,
+    // getMarktBasics,
+    // getOndernemer,
+    // getPlaatsvoorkeurenByMarktEnOndernemer,
+    // updateMarktVoorkeur,
+    // updatePlaatsvoorkeur,
+} from '../makkelijkemarkt-api';
+import {
+    getPlaatsvoorkeurenByMarktEnOndernemer,
+    updatePlaatsvoorkeur,
     deletePlaatsvoorkeurenByMarktAndKoopman,
     getIndelingVoorkeur,
-    getMarktBasics,
-    getOndernemer,
-    getPlaatsvoorkeurenByMarktEnOndernemer,
     updateMarktVoorkeur,
-    updatePlaatsvoorkeur,
-} from '../makkelijkemarkt-api';
+    getOndernemer,
+    getMarktBasics,
+} from '../daalder-api';
 import { getQueryErrors, HTTP_CREATED_SUCCESS, internalServerErrorPage } from '../express-util';
 import { NextFunction, Request, Response } from 'express';
 import { getKeycloakUser } from '../keycloak-api';
@@ -106,7 +115,7 @@ export const updatePlaatsvoorkeuren = (
 
             return updatePlaatsvoorkeur(voorkeuren, userEmail);
         } else {
-            return deletePlaatsvoorkeurenByMarktAndKoopman(marktId, erkenningsNummer);
+            return deletePlaatsvoorkeurenByMarktAndKoopman(marktId, erkenningsNummer, userEmail);
         }
     };
 
